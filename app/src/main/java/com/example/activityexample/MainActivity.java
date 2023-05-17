9abcef3be9e8fbcdeabab1b94d759aad92a56cc7
Udaka-Hayerson <mikehayerson@gmail.com>
2023-05-17 21:48:03 +0300

    two versions of the button implementation

--- app/src/main/java/com/example/activityexample/MainActivity.java
+++ app/src/main/java/com/example/activityexample/MainActivity.java
@@ -3,18 +3,32 @@ package com.example.activityexample;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
     private TextView text;
+    private TextView text_2;
+    private Button btn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
         text = (TextView) findViewById(R.id.textView);
+        text_2 = (TextView) findViewById(R.id.textView5);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = "it's works";
+                text_2.setText(s);
+                text_2.setTextColor(getResources().getColor(R.color.purple_200));
+                text_2.setBackgroundColor(getResources().getColor(R.color.black));
+            }
+        });
 
     }
 
