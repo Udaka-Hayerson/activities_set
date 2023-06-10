cc927360710710df624adc12b5e81cbb83c8b692
Udaka-Hayerson <mikehayerson@gmail.com>
2023-06-11 01:51:32 +0300

    adds fragment app example

--- app/src/main/java/com/example/activityexample/fifth_lesson/simple_fragments/AnswerFragment.java
+++ app/src/main/java/com/example/activityexample/fifth_lesson/simple_fragments/AnswerFragment.java
@@ -8,11 +8,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.example.activityexample.R;
+
 
 public class AnswerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate (R.layout.ex_001_answer_fragment, container, false);
     }
 }
--- app/src/main/java/com/example/activityexample/fifth_lesson/simple_fragments/MainActivity.java
+++ app/src/main/java/com/example/activityexample/fifth_lesson/simple_fragments/MainActivity.java
@@ -1,4 +1,39 @@
 package com.example.activityexample.fifth_lesson.simple_fragments;
 
-public class MainActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import com.example.activityexample.R;
+
+public class MainActivity extends Activity {
+
+    private TextView textView;
+    private EditText login;
+    private EditText password;
+    private String realLogin;
+    private String realPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex_001_main_layout);
+
+        realLogin = "log";
+        realPassword = "pass48";
+
+        textView = (TextView) findViewById(R.id.textView);
+        login = (EditText) findViewById(R.id.login);
+        password = (EditText) findViewById(R.id.password);
+
+    }
+        public void click(View view){
+            String loginTxt = login.getText().toString();
+            String passwordTxt = password.getText().toString();
+            String text = (loginTxt.equals(realLogin) && passwordTxt.equals(realPassword)) ? loginTxt + " you entred succsessful" : "incorrect password";
+            textView.setText(text);
+        }
+
 }
